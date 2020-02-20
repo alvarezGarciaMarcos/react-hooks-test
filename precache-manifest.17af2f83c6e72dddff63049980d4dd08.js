842466bb3da204ea99a7d7493480fd34e723d0a2
Marcos Alvarez <marcosalvarez@imac-de-marcos.home>
2020-02-20 01:07:04 +0100

    Updates

---
+++ precache-manifest.17af2f83c6e72dddff63049980d4dd08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281d8cde334689169d87b8ec3624782b",
+    "url": "/react-hooks-test/index.html"
+  },
+  {
+    "revision": "8964717a8f27219bef44",
+    "url": "/react-hooks-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a6b8060420bb41b49446",
+    "url": "/react-hooks-test/static/js/2.8a5dae5b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-hooks-test/static/js/2.8a5dae5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8964717a8f27219bef44",
+    "url": "/react-hooks-test/static/js/main.7a264e66.chunk.js"
+  },
+  {
+    "revision": "4609aa1b7d76a3d69814",
+    "url": "/react-hooks-test/static/js/runtime-main.db2c464d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-hooks-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
